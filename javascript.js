9f977c2bb9e9333100215b10d245934c55b8da07
Christian <christiancharlton17@gmail.com>
2023-01-22 15:31:42 -0700

    Create computer choice logic

--- javascript.js
+++ javascript.js
@@ -2,3 +2,18 @@
 function getRandomInt(min, max) {
     return Math.floor((Math.random() * (max - min) + min))
 }
+
+function getComputerChoice() {
+    let randomInt = getRandomInt(1, 4);
+    let choice = function (number) {
+        switch (number) {
+            case 1:
+                return "rock";
+            case 2:
+                return "paper";
+            case 3:
+                return "scissors";
+        }
+    }
+    return choice(randomInt)
+}
