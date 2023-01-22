6740795f585e4699d02184f5e9da7b6fa98d9721
Christian <christiancharlton17@gmail.com>
2023-01-22 15:30:35 -0700

    Create random integer generator function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-console.log("Hello I")
+// return a random integer between a specified minimum and maximum (range)
+function getRandomInt(min, max) {
+    return Math.floor((Math.random() * (max - min) + min))
+}
